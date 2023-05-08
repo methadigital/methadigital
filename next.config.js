abdbf62e1b984b96a31629fb30d51ab536d0a608
Natã Hermes <nhermesdev@gmail.com>
2023-05-08 17:09:02 -0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['page.tsx'],
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { CookiesProvider } from 'react-cookie';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <CookiesProvider>
+        <Head>
+          <title>Afiliados</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </CookiesProvider>
+    </>
+  );
+}
