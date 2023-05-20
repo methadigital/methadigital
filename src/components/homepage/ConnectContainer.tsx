9be1865bcb6cde79cda79c671725ae23ca4a3bd7
Metha Digital <118488356+methadigital@users.noreply.github.com>
2023-05-20 15:18:41 -0300

    Revert "commit test"

--- src/components/homepage/ConnectContainer.tsx
+++ src/components/homepage/ConnectContainer.tsx
@@ -5,6 +5,11 @@ export function ConnectContainer() {
   return (
     <FlexRowColStack>
       <div className="pt-16 pb-10">
+        <p className="text-xl md:text-2xl w-80 text-icons">
+          <span className="text-primary">Conecte-se</span> ao digital e
+          transforme seu futuro.
+        </p>
+
         <div className="mt-2 flex items-center cursor-pointer gap-2 text-icons hover:text-primary">
           <ArrowRightCircleIcon className="h-6" /> <span>Saiba mais sobre</span>
         </div>
